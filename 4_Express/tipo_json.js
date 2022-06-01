ce50423c9cd2c94d051e83d3bc7a2dbf4f4d3080
Jesus Gonzalez <jesusgonvale@gmail.com>
2022-05-31 20:51:06 -0500

    revisando los verbos en insonnia

--- 4_Express/tipo_json.js
+++ 4_Express/tipo_json.js
@@ -2,27 +2,27 @@ const express = require ( 'express' );
 
 const app = express();
  
-const port = 3500
+const port = 3500;
 
 app.get("/koder", (req, res) =>{
     const tipoJson ={
-        typo: 'Aqui estan todos los koders'
+        mensaje: 'Aqui estan todos los koders'
     } ;
     res.json(tipoJson)
-})
+});
 app.post("/koder", (req, res) =>{
     const tipoJson ={
-        typo: 'Aqui puedes crear koders'
+        mensaje: 'Aqui puedes crear koders'
     } ;
     res.json(tipoJson)
-})
+});
 app.put("/koder", (req, res) =>{
     const tipoJson ={
-        typo: 'Aqui puedes sustituir un koder'
+        mensaje: 'Aqui puedes sustituir un koder'
     } ;
     res.json(tipoJson)
-})
+});
 
 app.listen(port, () =>{
     console.log('corriendo servidor en el puerto', port)
-})
+});
