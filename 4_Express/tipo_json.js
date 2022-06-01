f4ce34600c390c81fc998f8e5ee6484ef1fdfefd
Jesus Gonzalez <jesusgonvale@gmail.com>
2022-05-31 22:16:54 -0500

    ejemplo del verbo post desde Express y guardado en koders.json

--- 4_Express/tipo_json.js
+++ 4_Express/tipo_json.js
@@ -1,15 +1,50 @@
 const express = require ( 'express' );
+const fs = require("fs/promises");
 
 const app = express();
+// middleware convierte request a json 
+app.use(express.json())
  
 const port = 3500;
+
+app.get("/koders", async (req, res) => {
+    const archivo = await fs.readFile("koders.json", "utf8"); //el archivo es un string
+    const kodersObjet = JSON.parse(archivo);
+    const koders = kodersObjet.koders;
+
+
+    res.json(koders);
+});
 
 app.get("/koder", (req, res) =>{
     const tipoJson ={
         mensaje: 'Aqui estan todos los koders'
     } ;
-    res.json(tipoJson)
+    res.json(tipoJson);
 });
+
+
+// aqui se crea el verbo post en express-------------------------------------------------------------
+
+app.post("/koders", async (req, res) => {
+    const koder = req.body;
+    const archivo = await fs.readFile("koders.json", "utf8"); //el archivo es un string
+    const kodersObjet = JSON.parse(archivo);
+    const koders = kodersObjet.koders;
+
+//   agregamos un nuevo koder
+    koders.push(koder);
+// guardamos cambios de los nuevos koders
+const nuevoArchivo = JSON.stringify(kodersObjet, null, 4);
+// escribimos en archivo asincrono, lleva el await
+await fs.writeFile("koders.json", nuevoArchivo, "utf8");
+
+// Aqui se envia la respuesta
+res.status(201); //estado creado 
+
+res.json(koders);
+    
+})
 app.post("/koder", (req, res) =>{
     const tipoJson ={
         mensaje: 'Aqui puedes crear koders'
