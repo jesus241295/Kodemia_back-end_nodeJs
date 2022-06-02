10d86f342a2009fbd5fe4f1753c6ceee0c98db2b
Jesus Gonzalez <jesusgonvale@gmail.com>
2022-06-01 21:39:30 -0500

    se suma el verbo delete para borrar los objetos conforme a su ruta

--- 4_Express/tipo_json.js
+++ 4_Express/tipo_json.js
@@ -44,7 +44,65 @@ res.status(201); //estado creado
 
 res.json(koders);
     
+});
+
+// app.patch("/koders/:nombre", async (req, res) => {
+// const nombre = req.params.nombre
+
+//     const koder = req.body;
+//     // console.log(koder)
+//     const archivo = await fs.readFile("koders.json", "utf8"); //el archivo es un string
+//     const kodersObjet = JSON.parse(archivo);
+//     const koders = kodersObjet.koders;
+    
+//    const newKoders =  koders.map((item) => {
+//         if(item.nombre === nombre){
+//          return koder
+//         }
+//         return item
+//     });
+
+    // app.delete("/koders/:nombre", async (req, res) => {
+    //     const nombre = req.params.nombre
+        
+    //         // const koder = req.body;
+    //         // console.log(koder)
+    //         const archivo = await fs.readFile("koders.json", "utf8"); //el archivo es un string
+    //         const kodersObjet = JSON.parse(archivo);
+    //         const koders = kodersObjet.koders;
+
+    //         const newKoders =  koders.filter((koder) => koder.nombre !== nombre);
+    // const newObject = {
+    //     koders: newKoders
+    // }
+    app.get("/koders/:nombre", async (req, res) => {
+        const nombre = req.params.nombre
+        
+            // const koder = req.body;
+            // console.log(koder)
+            const archivo = await fs.readFile("koders.json", "utf8"); //el archivo es un string
+            const kodersObjet = JSON.parse(archivo);
+            const koders = kodersObjet.koders;
+
+            const newKoders =  koders.filter((koder) => koder.nombre === nombre);
+    const newObject = {
+        koders: newKoders
+    }
+//   agregamos un nuevo koder
+    // koders.push(koder);
+// guardamos cambios de los nuevos koders
+const nuevoArchivo = JSON.stringify(newObject, null, 2);
+// escribimos en archivo asincrono, lleva el await
+await fs.writeFile("koders.json", nuevoArchivo, "utf8");
+
+// Aqui se envia la respuesta
+res.status(200); //estado creado 
+
+res.json(newKoders[0]);
+    
 })
+
+
 app.post("/koder", (req, res) =>{
     const tipoJson ={
         mensaje: 'Aqui puedes crear koders'
