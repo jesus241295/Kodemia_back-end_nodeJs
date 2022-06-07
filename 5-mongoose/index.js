328cd5b2834b882e62e647aad173bdef735302fd
Jesus Gonzalez <jesusgonvale@gmail.com>
2022-06-06 19:59:52 -0500

    .env

--- 5-mongoose/index.js
+++ 5-mongoose/index.js
@@ -1,13 +1,15 @@
+require ("dotenv").config();
 const mongoose = require ("mongoose");
 
-const DB_USER = 'jesus241295';
-const DB_PASS = 'jesus01.*';
-const DB_HOST = 'cluster0.smalkez.mongodb.net'
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_HOST = process.env.DB_HOST
 const URL =`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/?retryWrites=true&w=majority`;
 
 mongoose.connect(URL)
 .then((connection) => {
     console.log('estamos conectados a base de datos')
 }).catch((error) => {
-    console.error('no nos conectamos a base de datos')
+    console.error('no nos conectamos a base de datos');
+    console.error(error)
 });
